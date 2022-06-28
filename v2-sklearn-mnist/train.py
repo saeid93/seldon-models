6a0c06f592056dde260bfe6720011eb61b60736d
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2022-06-28 11:08:59 +0100

    add basic v2 mnist sklearn example

---
+++ v2-sklearn-mnist/train.py
@@ -0,0 +1,29 @@
+# Original source code and more details can be found in:
+# https://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+
+# The digits dataset
+digits = datasets.load_digits()
+
+# To apply a classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+# Create a classifier: a support vector classifier
+classifier = svm.SVC(gamma=0.001)
+
+# Split data into train and test subsets
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False)
+
+# We learn the digits on the first half of the digits
+classifier.fit(X_train, y_train)
+
+# Save model
+import joblib
+model_file_name = "mnist-svm.joblib"
+joblib.dump(classifier, model_file_name)
