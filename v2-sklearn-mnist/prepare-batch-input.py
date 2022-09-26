463a35b5c8914a24132283a1b100a75534a0e7cf
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2022-09-26 20:54:00 +0100

    add batch entrypoints

---
+++ v2-sklearn-mnist/prepare-batch-input.py
@@ -0,0 +1,36 @@
+import click
+import numpy as np
+import orjson
+
+
+def get_payload(shape, input_name="predict", datatype="FP32"):
+    data = np.random.rand(*shape)
+    return {
+        "inputs": [
+            {
+                "name": input_name,
+                "data": data.flatten().tolist(),
+                "datatype": datatype,
+                "shape": data.shape,
+            }
+        ]
+    }
+
+
+@click.command()
+@click.option("--batch-size", "-b", default=10, type=int)
+@click.option("--instances", "-i", default=500, type=int)
+@click.option("--file-name", "-f", default="batch-input.txt", type=str)
+def cli(batch_size, instances, file_name):
+    np.random.seed(0)
+
+    shape = (batch_size, 8, 8)
+    with open(file_name, "wb") as f:
+        for n in range(instances):
+            x = get_payload(shape)
+            f.write(orjson.dumps(x))
+            f.write(b"\n")
+
+
+if __name__ == "__main__":
+    cli()
